1c2cc54cc6b538217aa96b2e5213d9de9c539fc4
unknown <nyiri.martin.101@gmail.com>
2024-03-18 14:49:02 +0100

    movement

---
+++ move.gd
@@ -0,0 +1,24 @@
+extends MeshInstance3D
+
+var speed = 5  # Mozgás sebessége
+
+var velocity = Vector3()
+
+
+func _physics_process(delta):
+	velocity = Vector3.ZERO
+	
+	# Ellenőrizzük a billentyűleütéseket és állítsuk be a sebességvektort
+	if Input.is_action_pressed("a"):
+		velocity.x -= speed
+	if Input.is_action_pressed("d"):
+		velocity.x += speed
+	if Input.is_action_pressed("w"):
+		velocity.z -= speed
+	if Input.is_action_pressed("s"):
+		velocity.z += speed
+		
+	
+	if velocity.length_squared() > 0:
+		velocity = velocity.normalized() * speed
+	translate(velocity * delta)
